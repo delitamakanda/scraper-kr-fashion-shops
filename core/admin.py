ebe7567073df791be4c1d4f5e562dd300416f284
Délita Makanda <delita.makanda@gmail.com>
2022-05-08 01:18:47 +0200

    delete product from db 6 month later

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from core.models import Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'created']
+    search_fields = ['name', 'description']
+    list_filter = ['created', 'available']
+
+
+admin.site.register(Product, ProductAdmin)
