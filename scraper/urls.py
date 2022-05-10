c1596886349b4855ef52cc541c197ba58a9aa039
Délita Makanda <delitamakanda@users.noreply.github.com>
2022-05-10 16:48:35 +0200

    Update urls.py

--- scraper/urls.py
+++ scraper/urls.py
@@ -59,7 +59,7 @@ urlpatterns = [
     path('devices/', FCMDeviceViewSet.as_view({'post': 'create'}), name='create_fcm_device'),
     
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     # re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
