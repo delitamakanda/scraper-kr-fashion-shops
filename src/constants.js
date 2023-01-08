890bbc770126aa31d64317cf24d857fe559dd552
Délita Makanda <delita.makanda@gmail.com>
2023-01-08 22:58:19 +0100

    fix add /

--- src/constants.js
+++ src/constants.js
@@ -6,4 +6,4 @@ export const userMailingURL = `${endpoint}/signup/`;
 
 export const productListURL = `${endpoint}/products/`;
 export const productDetailURL = (productID) => `${endpoint}/products/${productID}/`;
-export const favProductsURL = (productListIDs) => `${endpoint}/products/?id__in=${productListIDs}`;
+export const favProductsURL = (productListIDs) => `${endpoint}/products/?id__in=${productListIDs}/`;
