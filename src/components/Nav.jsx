8e3810cd2117b5cce580e83c41c717c52bbc06ec
Délita Makanda <delita.makanda@gmail.com>
2023-03-26 18:47:52 +0200

    style(Nav): sticky nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,19 +6,21 @@ import { PhotographIcon, HeartIcon } from '@heroicons/react/outline'
 const Nav = (props) => {
     const { selected } = props
     return (
-        <div className='flex justify-center'>
-            <Link to="/">
-                <button className={selected === 'home' ? 'bg-pink-500 text-white p-2 rounded' : 'bg-white text-black p-2 rounded'}>
-                    <PhotographIcon className="h-5 w-5 currentColor" />
-                </button>
-            </Link>
-             <Link to="/favorites-products">
-                <button className={selected === 'fav' ? 'bg-pink-500 text-white p-2 rounded' : 'bg-white text-black p-2 rounded'
-}>
-                    <HeartIcon className="h-5 w-5 currentColor" />
-                </button>
-            </Link>
-        </div>              
+        <div className='sticky top-0 z-10 bg-white'>
+            <div className='flex justify-center'>
+                <Link to="/">
+                    <button className={selected === 'home' ? 'bg-pink-500 text-white p-2 rounded' : 'bg-white text-black p-2 rounded'}>
+                        <PhotographIcon className="h-5 w-5 currentColor" />
+                    </button>
+                </Link>
+                <Link to="/favorites-products">
+                    <button className={selected === 'fav' ? 'bg-pink-500 text-white p-2 rounded' : 'bg-white text-black p-2 rounded'
+    }>
+                        <HeartIcon className="h-5 w-5 currentColor" />
+                    </button>
+                </Link>
+            </div>              
+        </div>
     );
 };
 
