7f0b21422813276eb378f68f339bfdbfbb049838
Délita Makanda <delita.makanda@gmail.com>
2024-04-05 21:25:57 +0200

    fix(prod): edit title

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -11,7 +11,7 @@ const CustomLayout = (props) => {
                     <div className="flex items-center justify-between border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
                         <div className="flex justify-start lg:w-0 lg:flex-1">
                             <Link to="/">
-                                <span className="sr-only">Korean fashion</span>
+                                <span className="sr-only">KF</span>
                                 <img
                                 className="h-8 w-auto sm:h-10"
                                 src="/static/icon-48x48.png"
