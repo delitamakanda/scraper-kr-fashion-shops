b000a9840cfed57a964432391fe277ec7c6c8ec1
Délita Makanda <delita.makanda@gmail.com>
2024-11-08 01:22:55 +0100

    fix(scraper): fix price mapping

--- core/scrapers/scrapers_wonlog.py
+++ core/scrapers/scrapers_wonlog.py
@@ -4,6 +4,7 @@ import os
 import logging
 import time
 import random
+import re
 
 import chromedriver_autoinstaller
 from pathlib import Path
@@ -117,7 +118,20 @@ def parse_html(html):
             article['img'] = row.find('div', attrs= {'class': 'prdImg'}).img['src']
             article['title'] = row.find('div', class_='name').text.replace('\n', '')
             article['url'] = row.find('div', class_='thumbnail').a['href']
-            article['price'] = row.find('li', class_='xans-record-').find_all('span')[1].text.replace(',', '.')
+            ul_price = row.find('ul', class_='xans-element- xans-product xans-product-listitem spec')
+            if ul_price:
+                li_price = ul_price.find_all('li') if len(ul_price.find_all('li')) > 1 else None
+                for li in li_price:
+                    price_span = li.find_all('span', style=re.compile(r'font-size:11px;color:#000000;')) or li.find_all('span', style=re.compile(r'font-size:12px;color:#555555;'))
+                    for span in price_span:
+                        second_span = re.compile(r'\d+\,\d+').findall(span.text)
+                        for p in second_span:
+                            formatted_price = p.replace(',', '.')
+                            try:
+                                article['price'] = float(formatted_price)
+                            except ValueError:
+                                logger.error(f'Failed to parse price: {formatted_price}')
+                                article['price'] = '0.00'
             output_list.append(article)
             logger.info(f'Parsed article: {article["title"]}')
     return output_list
