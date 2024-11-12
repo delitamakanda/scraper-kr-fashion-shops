ef93f2e89f8a9f89184be0bfac14f094472b6fc1
Délita Makanda <delitamakanda@users.noreply.github.com>
2024-11-12 14:17:24 +0100

    fix: remove min-height

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -71,7 +71,7 @@ const Item = (props) => {
                     <LazyLoadImage src={item.image_url} alt={item.name} height={'100%'} width={'100%'} placeholdersrc={PlaceholderImage} className="w-full h-full object-center object-cover lg:w-full lg:h-full" effect="blur" />
                 
             </div>) : (
-                <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+                <div className="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
                     <LazyLoadImage src={item.image} alt={item.name} placeholdersrc={PlaceholderImage} className="w-full h-full object-center object-cover lg:w-full lg:h-full" effect="blur" />
                 </div>
             )}
