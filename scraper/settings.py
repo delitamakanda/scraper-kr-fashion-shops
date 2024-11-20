90b8fe9f695214e49600e3e723d754c9a89c231d
Délita Makanda <delita.makanda@gmail.com>
2024-11-20 23:34:44 +0100

    delete credentials

--- scraper/settings.py
+++ scraper/settings.py
@@ -194,11 +194,23 @@ FCM_DJANGO_SETTINGS = {
     "FCM_SERVER_KEY": os.getenv("FCM_SERVER_KEY", default="dummy"),
 }
 
-from firebase_admin import initialize_app, credentials
+if not DEBUG:
+    from firebase_admin import initialize_app, credentials
 
-cred = credentials.Certificate(os.path.join(os.path.join(BASE_DIR, "scraper", "credentials.json")))
-
-FIREBASE_APP = initialize_app(cred)
+    FIREBASE_APP = initialize_app({
+      credential=credentials.Certificate({
+          "type": "service_account",
+          "project_id": os.environ.get('FIREBASE_PROJECT_ID'),
+          "private_key_id": os.environ.get('PRIVATE_KEY_ID'),
+          "private_key": os.environ.get('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
+          "client_email": os.environ.get('FIREBASE_CLIENT_EMAIL'),
+          "client_id": os.environ.get('CLIENT_ID'),
+          "auth_uri": os.environ.get('AUTH_URI'),
+          "token_uri": os.environ.get('TOKEN_URI'),
+          "auth_provider_x509_cert_url": os.environ.get('AUTH_PROVIDER_X509_CERT_URL'),
+          "client_x509_cert_url": os.environ.get('CLIENT_X509_CERT_URL'),
+      }),
+    })
 
 # celery
 CELERY_BROKER_URL = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
