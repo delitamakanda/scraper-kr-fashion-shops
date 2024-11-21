65752e46160b858f204725b3ecd4fbd8a8353511
Délita Makanda <delita.makanda@gmail.com>
2024-11-21 01:00:13 +0100

    fix firebase admin

--- scraper/settings.py
+++ scraper/settings.py
@@ -195,23 +195,23 @@ FCM_DJANGO_SETTINGS = {
 }
 
 import firebase_admin
-import base64
+import json
+import decrypt
 from firebase_admin import initialize_app, credentials
 
 try:
-    base64_encoded = base64.b64encode(os.environ.get('FIREBASE_PRIVATE_KEY').replace('\\n', '\n').encode()).decode()
-    service_account_key = {
+    service_account_key = json.loads({
         "type": "service_account",
         "project_id": os.environ.get('FIREBASE_PROJECT_ID'),
         "private_key_id": os.environ.get('PRIVATE_KEY_ID'),
-        "private_key": base64.b64decode(base64_encoded).decode('utf-8'),
+        "private_key": os.environ.get('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
         "client_email": os.environ.get('FIREBASE_CLIENT_EMAIL'),
         "client_id": os.environ.get('CLIENT_ID'),
         "auth_uri": os.environ.get('AUTH_URI'),
         "token_uri": os.environ.get('TOKEN_URI'),
         "auth_provider_x509_cert_url": os.environ.get('AUTH_PROVIDER_X509_CERT_URL'),
         "client_x509_cert_url": os.environ.get('CLIENT_X509_CERT_URL'),
-    }
+    })
 
     cred = credentials.Certificate(service_account_key)
 
